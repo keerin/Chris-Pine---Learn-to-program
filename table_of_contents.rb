639f37969f9a83f8024af8a0090cd0bb6f7e80bb
keerin <thekieran@gmail.com>
2013-12-30 09:22:28 +0000

    Create table_of_contents.rb

---
+++ table_of_contents.rb
@@ -0,0 +1,13 @@
+lineWidth = 10
+puts "Table of Contents".center(lineWidth)
+print "Chapter 1:".ljust(lineWidth)
+print "Numbers".center(lineWidth)
+puts "page 1".rjust(lineWidth)
+print "Chapter 2:".ljust(lineWidth)
+print "NLetters".center(lineWidth)
+puts "page 72".rjust(lineWidth)
+print "Chapter 3:".ljust(lineWidth)
+print "Variables".center(lineWidth)
+print "page 118".rjust(lineWidth)
+
+#this could be better...
